3da651f84c3913f42d3bf7e5e7f5a2195cf9087b
Anthony <anthonypham@APhams-Macbook-Pro.local>
2023-03-21 11:14:01 -0700

    completed pair_sum example

---
+++ pair_sum.py
@@ -0,0 +1,8 @@
+def pair_sum(numbers, target_sum):
+    previous_nums = {}
+
+    for index, num in enumerate(numbers):
+        complement = target_sum - num
+
+        if complement in previous_nums:
+            return (previous_nums[complement], index)
