d371fa4d44d8e053a2881542e448a1114aecfae0
Anthony <apham.dev@gmail.com>
2023-08-08 11:07:17 -0700

    compress practice with psuedocode

--- compress.py
+++ compress.py
@@ -1,20 +1,38 @@
 def compress(s):
+    # add a special character to the string
     s += "!"
-    results = []
+    # initiate two pointer variables
     i = 0
     j = 0
-    while j < len(s):
-        if s[i] == s[j]:
-            j += 1
-        else:
-            num = j - i
-            if num == 1:
-                results.append(s[i])
-            else:
-                results.append(str(num) + s[i])
+      # initiate empty list
+    results = []
 
-            i = j
+      #while our second pointer is less than the the length of s
+    while j < len(s):
+    # if s[j] is pointing to the same letter as s[i]
+      if s[i] == s[j]:
+    # j += 1
+        j += 1
+    # else (once j is pointing to a letter that isn't the same as i):
+      else:
+      # initiate a number variable to be the difference of j - i
+        number = j - i
+        # we have to check if there is only one letter, we don't want it to show up as "1t", we just want it to show the letter
+        if number == 1:
+          results.append(s[i])
+        else:
+      # add the result of the number and the letter to the list
+          results.append(str(number) + s[i])
+        # catch up i pointer to j
+        i=j
+
+
+    # "".join(result)
     return ''.join(results)
+
+
+
+
 
 
 print(compress('ccaaatsss'))
