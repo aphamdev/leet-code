38dbd806129e5a31f79582787bb89a0bea7c5a22
Anthony <apham.dev@gmail.com>
2023-08-12 21:44:32 -0700

    five sorts test

--- five_sort.py
+++ five_sort.py
@@ -9,6 +9,15 @@
 #five_sort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5])
 # -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
 
+
+
+#fours = [4] * 20000
+# fives = [5] * 20000
+# nums = fours + fives
+# five_sort(nums)
+# twenty-thousand 4s followed by twenty-thousand 5s
+# -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
+
 def five_sort(nums):
     i = 0
     j = len(nums) - 1
